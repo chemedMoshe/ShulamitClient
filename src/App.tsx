c464ad4f94388bf10f501e008e9ab50182449a8f
mosheChemed <limudimIDF@gmail.com>
2024-12-16 20:57:31 +0200

    add navbar mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import PrimarySearchAppBar from "./components/Navbar";
 export default function App() {
   return (
     <div className="app">
-      
+     <PrimarySearchAppBar />
     </div>
   )
 }
