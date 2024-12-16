e9455eab4e3924e557ef54b0e4a682d306a62613
mosheChemed <limudimIDF@gmail.com>
2024-12-16 23:38:16 +0200

    /

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import PrimarySearchAppBar from "./components/Navbar";
 export default function App() {
   return (
     <div className="app">
+      <div className="navbar">
      <PrimarySearchAppBar />
+     </div>
+     <div className="content">
+
+     </div>
     </div>
   )
 }
