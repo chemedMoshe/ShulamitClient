16d44a2e71efc08f2460e924a37ad31f9cd3bebf
mosheChemed <limudimIDF@gmail.com>
2024-12-30 17:34:07 +0200

    change lable input login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import MuiCard from '@mui/material/Card';
-import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
-import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
@@ -34,7 +32,6 @@ export default function SignInCard() {
     const [passwordError, setPasswordError] = React.useState(false);
     const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
     const navigate = useNavigate();
-
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         if (emailError || passwordError) {
@@ -75,16 +72,16 @@ export default function SignInCard() {
         return isValid;
     };
 
-const clearErrors = (e:string|null) => {
-    if(e) return
-    setEmailError(false);
-    setEmailErrorMessage('');
-    setPasswordError(false);
-    setPasswordErrorMessage('');
-}
+    const clearErrors = (e: string | null) => {
+        if (e) return;
+        setEmailError(false);
+        setEmailErrorMessage('');
+        setPasswordError(false);
+        setPasswordErrorMessage('');
+    };
 
     return (
-        <Card variant="outlined" className='Login' sx={{ width:"70%" }}>
+        <Card variant="outlined" className="Login" sx={{ width: "70%" }}>
             <Typography
                 component="h1"
                 variant="h4"
@@ -99,14 +96,13 @@ const clearErrors = (e:string|null) => {
                 sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
             >
                 <FormControl>
-                    <FormLabel htmlFor="email">Email</FormLabel>
                     <TextField
                         error={emailError}
                         helperText={emailErrorMessage}
                         id="email"
                         type="email"
                         name="email"
-                        placeholder="your@email.com"
+                        label="Email"
                         autoComplete="email"
                         autoFocus
                         required
@@ -117,25 +113,13 @@ const clearErrors = (e:string|null) => {
                     />
                 </FormControl>
                 <FormControl>
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <Link
-                            component="button"
-                            type="button"
-                            onClick={() => { }}
-                            variant="body2"
-                            sx={{ alignSelf: 'baseline' }}
-                        >
-                            Forgot your password?
-                        </Link>
-                    </Box>
                     <TextField
                         error={passwordError}
                         helperText={passwordErrorMessage}
                         name="password"
-                        placeholder="••••••"
                         type="password"
                         id="password"
+                        label="Password"
                         autoComplete="current-password"
                         autoFocus
                         required
@@ -156,7 +140,6 @@ const clearErrors = (e:string|null) => {
                     </a>
                 </Typography>
             </Box>
-
         </Card>
     );
 }
