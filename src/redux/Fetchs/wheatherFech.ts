684bf379dc8dcf00c71082eaa81f8980520881fc
Moshe <m0504136536@gmail.com>
2025-05-04 00:40:57 +0300

    deleted console.log

--- src/redux/Fetchs/wheatherFech.ts
+++ src/redux/Fetchs/wheatherFech.ts
@@ -2,7 +2,6 @@ export const checkWeather = async (lat: number, lon: number) => {
     try {
         const response = await fetch(`${import.meta.env.VITE_WEATHER_API_URL}/?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}${import.meta.env.VITE_WEATHER_API_UNITS}`);
         const data = await response.json();
-        console.log(data);
         return data
     } catch (error) {
         throw new Error("Error fetching weather data: " + error);
