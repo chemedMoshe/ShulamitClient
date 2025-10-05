d9b85651d3c1151493289e6e7b709e13e09be6fc
Moshe <chemedstorage@gmail.com>
2025-10-05 21:48:29 +0300

    change status login catch to right status

---
+++ src/Utils/getPayloadFromToken.ts
@@ -0,0 +1,17 @@
+export const extractPayloadFromToken = (token: string): any => {
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const decoded = atob(base64);
+    const jsonPayload = decodeURIComponent(
+      decoded
+        .split("")
+        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  } catch (error) {
+    console.error("Invalid token format", error);
+    return null;
+  }
+};
